893316ed4041ea93a9fb4463f5cf9cb8bc72845c
Ainara <ainarasantana7@gmail.com>
2023-06-19 10:28:47 +0100

    travelCard done

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+
+import TravelCard from '../../components/TravelCard/TravelCard'
 function Dashboard() {
   return (
    <> 
-    <Header/> 
+    <TravelCard/>
   </>
   )
 }
